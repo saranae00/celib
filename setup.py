73cbccb06506c0aa29ea08f6d3a4b409de669f90
cekwak <cekwak@dtech.ai>
2021-04-15 10:59:27 +0900

    수정함:        setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", encoding="utf-8") as f:  # README.md 내용 읽어오기
     long_description = f.read()
 setup(
     name="celib",
-    version="0.0.4",
+    version="0.0.5",
     description="celib",
     author="cekwak",
     author_email="saranae00@gmail.com",
     url="https://github.com/saranae00/celib",
-    download_url="https://github.com/saranae00/celib/archive/refs/tags/0.0.4.tar.gz",
+    download_url="https://github.com/saranae00/celib/archive/refs/tags/0.0.5.tar.gz",
     install_requires=["pandas", "numpy", "pathos"],
     packages=find_packages(exclude=[]),
     keywords=["celib"],
